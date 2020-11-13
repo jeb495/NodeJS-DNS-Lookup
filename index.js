8c7692a1603e668ea748f3ac577bbc08ff710bee
Jesse <54542639+jeb495@users.noreply.github.com>
2020-11-12 16:03:35 -0800

    Added Comments

--- index.js
+++ index.js
@@ -2,20 +2,25 @@ const http = require('http');
 const dns = require('dns');
 const readline = require("readline");
 
+
+//Establishing A Host Name and a Port
 const hostname = '127.0.0.1';
 const port = 3000;
 
+
+//Declaring Options
 const options = {
     family: 6,
     hints: dns.ADDRCONFIG | dns.V4MAPPED,
   };
 
-
+//Creating Command Line Interface
   const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
+//Prompting User for Input
 rl.question("Enter a domain to lookup: ", function(thedns) {
     dns.lookup(thedns, options, (err, address, family) =>
     console.log('IP ADRESS: %j FAMILY: IPv%s', address, family));
